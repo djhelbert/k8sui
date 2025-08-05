27b683c2aee92b497c3640201b1e365f4ab49fc2
Derek Helbert <derek.helbert@plexusworldwide.com>
2025-08-05 09:35:45 -0700

    Added name space model

---
+++ app/src/main/java/org/k8sui/ui/NameSpaceModel.java
@@ -0,0 +1,62 @@
+package org.k8sui.ui;
+
+import org.k8sui.model.NameSpace;
+
+import javax.swing.table.AbstractTableModel;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+
+public class NameSpaceModel extends AbstractTableModel {
+
+    private static final String[] headers = {"UID", "Namespace", "Creation Date", "Status"};
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private List<NameSpace> namespaces;
+
+    public NameSpaceModel(List<NameSpace> nodes) {
+        setNodes(nodes);
+    }
+
+    public void setNodes(List<NameSpace> nodes) {
+        this.namespaces = nodes;
+        Collections.sort(nodes);
+    }
+
+    @Override
+    public int getRowCount() {
+        return namespaces.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return headers.length;
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return headers[col];
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        if (col == 0) {
+            return namespaces.get(row).getUid();
+        } else if (col == 1) {
+            return namespaces.get(row).getNamespace();
+        } else if (col == 2) {
+            if (namespaces.get(row).getCreation() == null) {
+                return "";
+            }
+
+            return formatter.format(namespaces.get(row).getCreation());
+        }
+
+        return namespaces.get(row).getStatus();
+    }
+
+    @Override
+    public Class<String> getColumnClass(int col) {
+        return String.class;
+    }
+}
--- app/src/main/java/org/k8sui/ui/NameSpacePanel.java
+++ app/src/main/java/org/k8sui/ui/NameSpacePanel.java
@@ -1,6 +1,56 @@
 package org.k8sui.ui;
 
-import javax.swing.*;
+import io.kubernetes.client.openapi.ApiException;
+import org.k8sui.service.NameSpaceService;
 
-public class NameSpacePanel extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NameSpacePanel extends JPanel implements ActionListener {
+    JPanel buttonPanel = new JPanel();
+    JButton refreshButton = new JButton("Refresh");
+    JTable table;
+    NameSpaceModel nameSpaceModel;
+    NameSpaceService service = new NameSpaceService();
+
+    public NameSpacePanel() {
+        super();
+        init();
+    }
+
+    private void init() {
+        try {
+            nameSpaceModel = new NameSpaceModel(service.nameSpaces());
+        } catch (ApiException err) {
+            throw new RuntimeException(err);
+        }
+
+        refreshButton.addActionListener(this);
+        refreshButton.setIcon(Util.getImageIcon("undo.png"));
+
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.add(refreshButton);
+
+        table = new JTable(nameSpaceModel);
+
+        setLayout(new BorderLayout());
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+        add(buttonPanel, BorderLayout.NORTH);
+        add(new JScrollPane(table), BorderLayout.CENTER);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(refreshButton)) {
+            try {
+                nameSpaceModel.setNodes(service.nameSpaces());
+                nameSpaceModel.fireTableDataChanged();
+            } catch (ApiException err) {
+                throw new RuntimeException(err);
+            }
+        }
+    }
 }
